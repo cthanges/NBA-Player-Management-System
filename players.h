de5863a6336a6ba2a6e9ca1dbc26c42f8368c96b
cthanges <charranthangeswaran@gmail.com>
2025-07-21 17:40:18 -0400

    Added the players header file

---
+++ players.h
@@ -0,0 +1,26 @@
+#ifndef PLAYERS_H
+#define PLAYERS_H
+
+//Player data
+typedef struct{
+	char name[50];
+	int age;
+	int season;
+	char team[50];
+	int jerseyNumber;
+	float height;
+	float weight;
+	float ppg; //Points Per Game
+	float rpg; //Rebounds Per Game
+	float apg; //Assists Per Game
+	float spg; //Steals Per Game
+	float bpg; //Blocks Per Game
+}Player;
+
+FILE* openPlayerFile(const char* mode); //Open the player file
+
+//Feature function declarations
+void addPlayer();
+void displayPlayers();
+
+#endif
